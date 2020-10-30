41cb256726959db51387b9e366ced659e5f6fcea
wooos <819844849@qq.com>
2020-10-30 14:34:53 +0800

    [autoclean] add log and fix now time timezone

--- README.md
+++ README.md
@@ -9,8 +9,8 @@ autoclean:
   paths:
     - dirctory: "/data/logs"  # absolute path
       filename: "*.log"       # filename (eg test.log *.log)
-      command: gzip           # command (only gzip or rm)
-      expire: 15              # exrire days
+      command: gzip           # command (only gzip or remove)
+      expire: 15              # expire days
 ```  
 
 ## Run
--- utils/clean.py
+++ utils/clean.py
@@ -1,8 +1,12 @@
 import os
 import glob
 import gzip
+import logging
 from datetime import datetime, timedelta
 from .config import CleanConfig
+
+logging.basicConfig(level=logging.INFO, format='[%(levelname)s][%(asctime)s] %(message)s')
+log = logging.getLogger(__name__)
 
 
 class CleanAction(object):
@@ -20,18 +24,21 @@ class CleanAction(object):
         """
         for filename in glob.glob(r'{}/{}'.format(clean_path.directory, clean_path.filename)):
             mtime = datetime.utcfromtimestamp(os.stat(filename).st_mtime)
-            now_time = datetime.now()
+            now_time = datetime.utcnow()
             if mtime + timedelta(days=clean_path.expire) < now_time:
                 if clean_path.command == 'gzip':
+                    log.info('gzip file: {}'.format(filename))
                     with open(filename, 'rb') as f:
                         g = gzip.GzipFile(mode='wb', compresslevel=9, fileobj=open('{}.gz'.format(filename), 'wb'))
                         g.write(f.read())
                         g.close()
                     os.remove(filename)
                 elif clean_path.command == 'rm':
+                    log.info('remove file: {}'.format(filename))
                     os.remove(filename)
                 else:
-                    exit('Unknown command: {}, only support gzip and rm'.format(clean_path.command))
+                    exit('Unknown command: {}, only support gzip and remove'.format(clean_path.command))
+        log.info('Completion')
 
     def run(self):
         for clean_path in self.clean_config.paths:
